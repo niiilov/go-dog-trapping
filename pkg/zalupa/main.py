5f53fc1d1e0b7cf0aa823c01530ccf09409d757c
Pusya <mranimatory@gmail.com>
2025-09-09 16:35:35 +0300

    base genereation wordk

--- pkg/zalupa/main.py
+++ pkg/zalupa/main.py
@@ -1,9 +1,15 @@
 from docx import Document
 from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.section import WD_ORIENT
 
 # Создаем документ
 doc = Document()
+
+section = doc.sections[0]
+section.orientation = WD_ORIENT.LANDSCAPE
+section.page_width, section.page_height = section.page_height, section.page_width
+
 
 # Верхняя часть (две колонки: слева Администрация, справа ИП)
 table_header = doc.add_table(rows=1, cols=2)
@@ -41,14 +47,17 @@ subtitle = doc.add_paragraph(
 subtitle.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 # Таблица 4 столбца
-table = doc.add_table(rows=2, cols=4)
+table = doc.add_table(rows=2, cols=7)
 table.style = 'Table Grid'
 
 headers = [
     "Территориальный отдел",
     "Сведения о заявителе",
     "Место нахождения животного",
-    "Сведения о количестве животных без владельцев, поведение, срочность"
+    "Сведения о количестве животных без владельцев",
+    "Поведение",
+    "Срочность",
+    "Контактное лицо в территориальном отделе"
 ]
 
 for i, text in enumerate(headers):
@@ -58,9 +67,12 @@ for i, text in enumerate(headers):
 
 data = [
     "Введенский территориальный отдел",
-    "И.о. начальника Введенского территориального отдела И.В.Денисов , заявка от 18.08.2025 №348",
+    "И.о. начальника Введенского территориального отдела И.В.Денисов",
     "Липецкий округ, с.Воскресеновка, ул.Наречная в районе д.53",
-    "Количество: 1\nПоведение: агрессивное\nСрочность: срочно"
+    "1", 
+    "Агрессивное", 
+    "Срочно",
+    "И.о. начальника Введенского территориального отдела Денисов И.В. т. 75-60-21"
 ]
 
 for i, text in enumerate(data):
@@ -69,11 +81,10 @@ for i, text in enumerate(data):
     cell.paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 # Контакты
-doc.add_paragraph("\nКонтактное лицо: И.о. начальника Введенского территориального отдела Денисов И.В. т. 75-60-21")
 
 # Подпись
-doc.add_paragraph("\nЗаместитель председателя комитета энергетики и ЖКХ\nадминистрации Липецкого муниципального округа")
-doc.add_paragraph("Е.А. Челидина\n\n34-57-29")
+doc.add_paragraph("\nЗаместитель председателя комитета энергетики и ЖКХ\nадминистрации Липецкого муниципального округа                                      Е.А. Челидина")
+doc.add_paragraph("34-57-29")
 
 # Сохраняем документ
 doc.save("zayavka.docx")
